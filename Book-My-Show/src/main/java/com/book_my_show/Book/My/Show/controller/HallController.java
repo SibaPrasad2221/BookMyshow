790f5c2af17f37bacc06b388cc8b5bdfeb920545
sahoo <sahoosibaprasad2221@gmail.com>
2023-12-29 00:54:32 +0530

    Add screen functionality

--- Book-My-Show/src/main/java/com/book_my_show/Book/My/Show/controller/HallController.java
+++ Book-My-Show/src/main/java/com/book_my_show/Book/My/Show/controller/HallController.java
@@ -1,6 +1,7 @@
 package com.book_my_show.Book.My.Show.controller;
 
 import com.book_my_show.Book.My.Show.dto.request.AddScreenDTO;
+import com.book_my_show.Book.My.Show.dto.request.AddShowDTO;
 import com.book_my_show.Book.My.Show.dto.request.HallOwner_SignUp_DTO;
 import com.book_my_show.Book.My.Show.dto.request.MovieOwner_SignUp_DTO;
 import com.book_my_show.Book.My.Show.dto.response.GeneralMessageDTO;
@@ -8,6 +9,7 @@ import com.book_my_show.Book.My.Show.exception.ResourceNotExistException;
 import com.book_my_show.Book.My.Show.exception.UnAuthorizedException;
 import com.book_my_show.Book.My.Show.exception.UserDoesNotExistException;
 import com.book_my_show.Book.My.Show.models.ApplicationUser;
+import com.book_my_show.Book.My.Show.models.Show_ent;
 import com.book_my_show.Book.My.Show.service.HallService;
 import com.book_my_show.Book.My.Show.service.MovieService;
 import org.apache.catalina.User;
@@ -39,15 +41,24 @@ public class HallController {
             return new ResponseEntity(new GeneralMessageDTO(e.getMessage()), HttpStatus.UNAUTHORIZED); //401
         }catch(ResourceNotExistException e){ //if a hall owner is wrongly want to access different hall or hall is not cumes under that user
             return new ResponseEntity(new GeneralMessageDTO(e.getMessage()), HttpStatus.NOT_FOUND); //404 //rosroucces nto found
-
         }
 
         return new ResponseEntity(new GeneralMessageDTO("Screen added successfully"), HttpStatus.CREATED); //201
     }
 
     @PostMapping("/addShow")
-    public ResponseEntity createShow(@RequestBody AddScreenDTO addScreenDTO, @RequestParam String email){
-        return new ResponseEntity(HttpStatus.CREATED);
+    public ResponseEntity createShow(@RequestBody AddShowDTO addShowDTO, @RequestParam String email){
+        try{
+            Show_ent showEnt = hallService.createShow(addShowDTO, email);
+            return new ResponseEntity(showEnt, HttpStatus.CREATED);
+        }catch(UserDoesNotExistException e){
+            return new ResponseEntity(new GeneralMessageDTO(e.getMessage()), HttpStatus.NOT_FOUND); //404
+        }catch (UnAuthorizedException e){
+            return new ResponseEntity(new GeneralMessageDTO(e.getMessage()), HttpStatus.UNAUTHORIZED); //401
+        }catch(ResourceNotExistException e){ //if a hall owner is wrongly want to access different hall or hall is not cumes under that user
+            return new ResponseEntity(new GeneralMessageDTO(e.getMessage()), HttpStatus.NOT_FOUND); //404 //rosroucces nto found
+        }
+
     }
 
 
